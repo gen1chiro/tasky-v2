10eb2b53e8bacde7510616236dcf72388fa88767
Jul Leo Javellana <juljavellana1218@gmail.com>
2025-07-26 15:33:13 +0800

    feat: delete task & column confirmation modals

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,12 +1,18 @@
-import { useDroppable } from "@dnd-kit/core"
-import { useSortable, SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable"
-import { CSS } from "@dnd-kit/utilities"
-import { deleteColumn, editColumn } from "../firebase/firestore/columns.ts"
-import { addTask } from "../firebase/firestore/tasks.ts"
+import {useRef} from "react";
+import {useDroppable} from "@dnd-kit/core"
+import {useSortable, SortableContext, verticalListSortingStrategy} from "@dnd-kit/sortable"
+import {CSS} from "@dnd-kit/utilities"
+import {deleteColumn, editColumn} from "../firebase/firestore/columns.ts"
+import {addTask} from "../firebase/firestore/tasks.ts"
 import Task, {TaskPreview} from "./Task.tsx"
-import type { Column } from "../types/types.ts"
+import type {Column} from "../types/types.ts"
+import Modal from "./modal/Modal.tsx"
+import ModalHeader from "./modal/ModalHeader.tsx"
+import ModalMessage from "./modal/ModalMessage.tsx"
 
-const Column = ({column, tasks, boardId, columnName, taskName, setTaskName}: {column: Column}) => {
+const Column = ({column, tasks, boardId, columnName, taskName, setTaskName}: { column: Column }) => {
+    const deleteModalRef = useRef<HTMLDialogElement | null>(null)
+
     const {setNodeRef: setDroppableRef} = useDroppable({
         id: column.id
     })
@@ -27,41 +33,68 @@ const Column = ({column, tasks, boardId, columnName, taskName, setTaskName}: {co
         transition,
     }
 
-    return (
-        <div
-            style={style}
-            ref={setNodeRef}
-            className={`flex flex-col items-center justify-center gap-4 w-80 bg-slate-300 p-4 ${isDragging ? 'opacity-50' : ''}`}>
-            <div
-                {...attributes}
-                {...listeners}
-            >drag
-            </div>
-            <h2 className='text-center font-bold'>{column.name}</h2>
-            <div className='flex justify-center gap-2'>
-                <button onClick={() => editColumn(boardId as string, column.id, columnName, "name")}
-                        className='bg-white rounded-md px-2'>Rename
-                </button>
-                <button onClick={() => deleteColumn(boardId as string, column.id)}
-                        className='bg-white rounded-md px-2'>Delete
-                </button>
-            </div>
-            <div className='w-full flex gap-2 justify-center'>
-                <input type='text' value={taskName} onChange={(e) => setTaskName(e.target.value)}
-                       className='bg-white flex-shrink'/>
-                <button onClick={() => addTask(boardId as string, column.id, taskName)}
-                        className='bg-white rounded-md px-2'>add
-                </button>
-            </div>
-            <SortableContext items={tasks} strategy={verticalListSortingStrategy}>
-                <div ref={setDroppableRef} className="w-full bg-white min-h-56">
-                    {tasks.map((task) => (
-                            <Task key={task.id} task={task} boardId={boardId} column={column} taskName={taskName}/>
-                    ))}
-                </div>
-            </SortableContext>
-        </div>
+    const showDeleteModal = () => {
+        deleteModalRef.current?.showModal()
+    }
 
+    const hideDeleteModal = () => {
+        deleteModalRef.current?.close()
+    }
+
+    const handleDelete = async () => {
+        await deleteColumn(boardId as string, column.id)
+    }
+
+    return (
+        <>
+            <div
+                style={style}
+                ref={setNodeRef}
+                className={`flex flex-col items-center justify-center gap-4 w-80 bg-slate-300 p-4 ${isDragging ? 'opacity-50' : ''}`}>
+                <div
+                    {...attributes}
+                    {...listeners}
+                >drag
+                </div>
+                <h2 className='text-center font-bold'>{column.name}</h2>
+                <div className='flex justify-center gap-2'>
+                    <button onClick={() => editColumn(boardId as string, column.id, columnName, "name")}
+                            className='bg-white rounded-md px-2'>Rename
+                    </button>
+                    <button onClick={showDeleteModal}
+                            className='bg-white rounded-md px-2'>Delete
+                    </button>
+                </div>
+                <div className='w-full flex gap-2 justify-center'>
+                    <input type='text' value={taskName} onChange={(e) => setTaskName(e.target.value)}
+                           className='bg-white flex-shrink'/>
+                    <button onClick={() => addTask(boardId as string, column.id, taskName)}
+                            className='bg-white rounded-md px-2'>add
+                    </button>
+                </div>
+                <SortableContext items={tasks} strategy={verticalListSortingStrategy}>
+                    <div ref={setDroppableRef} className="w-full bg-white min-h-56">
+                        {tasks.map((task) => (
+                            <Task key={task.id} task={task} boardId={boardId} column={column} taskName={taskName}/>
+                        ))}
+                    </div>
+                </SortableContext>
+            </div>
+
+            <Modal ref={deleteModalRef}>
+                <ModalHeader>Are you sure you want to delete this column?</ModalHeader>
+                <ModalMessage>This column along with its respective tasks will be deleted. This action cannot be
+                    undone.</ModalMessage>
+                <div className='w-full flex justify-end gap-2 mt-4'>
+                    <button onClick={hideDeleteModal}
+                            className='border-gray-300 border-1 px-6 py-1 rounded text-sm hover:bg-gray-100'>Cancel
+                    </button>
+                    <button onClick={handleDelete}
+                            className='bg-red-500 px-6 py-1 rounded text-white text-sm hover:bg-red-600'>Delete
+                    </button>
+                </div>
+            </Modal>
+        </>
     )
 }
 
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,8 +1,14 @@
-import { useSortable } from "@dnd-kit/sortable"
-import { CSS } from "@dnd-kit/utilities"
-import { deleteTask, editTask } from "../firebase/firestore/tasks.ts"
+import {useRef} from "react";
+import {useSortable} from "@dnd-kit/sortable"
+import {CSS} from "@dnd-kit/utilities"
+import {deleteTask, editTask} from "../firebase/firestore/tasks.ts"
+import Modal from "./modal/Modal.tsx"
+import ModalHeader from "./modal/ModalHeader.tsx"
+import ModalMessage from "./modal/ModalMessage.tsx"
 
 const Task = ({task, boardId, taskName}) => {
+    const deleteModalRef = useRef<HTMLDialogElement | null>(null)
+
     const {
         attributes,
         listeners,
@@ -22,23 +28,44 @@ const Task = ({task, boardId, taskName}) => {
         transition,
     }
 
+    const showDeleteModal = () => {
+        deleteModalRef.current?.showModal()
+    }
+
+    const hideDeleteModal = () => {
+        deleteModalRef.current?.close()
+    }
+
+    const handleDelete = async () => {
+        await deleteTask(boardId, task.columnId, task.id)
+    }
+
     return (
-        <div >
-            <div
-                ref={setNodeRef}
-                style={style}
-                {...attributes}
-                {...listeners}
-                className={`p-2 rounded-md shadow-md flex flex-col items-center gap-2 bg-slate-200 ${isDragging ? 'opacity-50' : ''}`}
-            >
-                <h1>{task.name}</h1>
+        <>
+            <div>
+                <div
+                    ref={setNodeRef}
+                    style={style}
+                    {...attributes}
+                    {...listeners}
+                    className={`p-2 rounded-md shadow-md flex flex-col items-center gap-2 bg-slate-200 ${isDragging ? 'opacity-50' : ''}`}
+                >
+                    <h1>{task.name}</h1>
+                </div>
+                <button onClick={showDeleteModal}>delete</button>
+                <button
+                    onClick={() => editTask(boardId, task.columnId, task.id, taskName, "name")}>rename
+                </button>
             </div>
-            <button onClick={() => deleteTask(boardId, task.columnId, task.id)}>delete
-            </button>
-            <button
-                onClick={() => editTask(boardId, task.columnId, task.id, taskName, "name")}>rename
-            </button>
-        </div>
+            <Modal ref={deleteModalRef}>
+                <ModalHeader>Are you sure you want to delete this task?</ModalHeader>
+                <ModalMessage>This action cannot be undone.</ModalMessage>
+                <div className='w-full flex justify-end gap-2 mt-4'>
+                    <button onClick={hideDeleteModal} className='border-gray-300 border-1 px-6 py-1 rounded text-sm hover:bg-gray-100'>Cancel</button>
+                    <button onClick={handleDelete} className='bg-red-500 px-6 py-1 rounded text-white text-sm hover:bg-red-600'>Delete</button>
+                </div>
+            </Modal>
+        </>
     )
 }
 
